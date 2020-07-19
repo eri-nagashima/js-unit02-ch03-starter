9100d57645fe63728a95b2ace5b08a783b8f3be5
Eri Nagashima <es6.ukf@gmail.com>
2020-07-19 16:48:59 +0900

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -5,17 +5,16 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: './bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
+  mode: 'development',
   devServer: {
     hot: true,
     hotOnly: true,
     watchContentBase: true,
     watchOptions: {
-      ignored: /node_modules/
-    }
+      ignored: /node_modules/,
+    },
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
+  plugins: [new webpack.HotModuleReplacementPlugin()],
 };
