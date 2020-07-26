1601f01a14c17c1857cecdacea28d06d4bd0265a
Eri Nagashima <es6.ukf@gmail.com>
2020-07-26 17:15:47 +0900

    Update

--- src/index.js
+++ src/index.js
@@ -34,20 +34,20 @@ function getData() {
     失敗ならエラーメッセージをPromise.rejectで返します。
   */
   return fetchData().then((response) => {
-    console.log(response); //確認用 statusは403になる
     const jsonData = response.json();
-    console.log(jsonData); //確認用 if文に抜けると思いきや、else文に抜けているため、Cannot read property 'propertyName' of undefinedと表示されてしまう（添付参照）
 
-    if (!response.status === 200) {
-      return Promise.reject(jsonData);
+    if (response.status !== 200) {
+      jsonData.then((err) => {
+        return Promise.reject(err);
+      });
     } else {
-      return Promise.resolve(jsonData); //こちらに抜けているため、エラー？
+      return Promise.resolve(jsonData);
     }
   });
 }
 
-function fetchData(id = 1) {
-  const url = 'http://localhost:3000/properties/2'; //エラーメッセージ検証用URL
+function fetchData(id = 2) {
+  const url = `${endpoint}/properties/${id}`;
   /* 
     fetchを使ってデータを取得します。
   */
